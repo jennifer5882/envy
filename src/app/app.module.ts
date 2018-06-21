1083aaa4b64953a292fdb9061b06901338fd9b16
Jennifer Sturgis <jennifernsturgis@gmail.com>
2018-06-20 18:23:07 -0700

    footer changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,34 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { ContentComponent } from './content/content.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ReturnsComponent } from './returns/returns.component';
+import { PrivacyComponent } from './privacy/privacy.component';
+import { TermsComponent } from './terms/terms.component';
+import { RouterModule, Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'privacy', component: PrivacyComponent },
+  { path: 'terms', component: TermsComponent },
+  { path: 'returns', component: ReturnsComponent },
+  //{ path: 'faq', component: FAQComponent },
+  { path: 'content', component: ContentComponent },
+  { path: '', redirectTo: '/content', pathMatch: 'full' }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    ReturnsComponent,
+    PrivacyComponent,
+    TermsComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}
+    ),
     BrowserModule,
     MatSelectModule,
     MatButtonModule,
